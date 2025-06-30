cf0751aa9e148fe3d7f01acdd78aa6232b310beb
Paul <ibeabuchi@worklio.cz>
2025-06-30 13:47:55 +0100

    implemented all checked

--- src/components/pages/settings-page/UsersTable.tsx
+++ src/components/pages/settings-page/UsersTable.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { useFetchAPI } from "../../../api/useFetchAPI";
 import type { UsersDataType } from "../../../types/UsersDataType";
 import UsersTableRow from "./UsersTableRow";
 
 const UsersTable = () => {
+  const [isAllChecked, setIsAllChecked] = useState(false);
+
   const url = "https://gamma-api.vercel.app/api/roles";
   const { data, isLoading, error } = useFetchAPI<UsersDataType[]>(url);
 
@@ -14,6 +17,7 @@ const UsersTable = () => {
       date={item.date}
       status={item.status.replace(" ", "")}
       users={item.users}
+      isAllChecked={isAllChecked}
       key={item.id}
     />
   ));
@@ -24,7 +28,15 @@ const UsersTable = () => {
       {!isLoading && !error && (
         <div className="bg-white border border-gray-300 shadow-gray-200 shadow-md rounded-lg max-h-[500px] overflow-y-scroll">
           <div className="flex items-center sm:divide-x sm:divide-gray-200 sticky top-0 justify-between px-6 py-3.25 border-b border-gray-200 text-gray-500 text-sm bg-gray-50 z-10">
-            <p className="w-fit sm:w-[25%]">Name</p>
+            <div className="w-fit sm:w-[25%] flex items-center gap-3">
+              <input
+                type="checkbox"
+                checked={isAllChecked}
+                onChange={(e) => setIsAllChecked(e.target.checked)}
+                className="accent-purple-600 w-5 h-5 rounded-lg border-gray-200 outline-purple-500/20 cursor-pointer"
+              />
+              <p>Name</p>
+            </div>
             <p className="w-[15%] hidden sm:flex">Type</p>
             <p className="w-fit sm:w-[10%]">Date Created</p>
             <p className="w-[66px] hidden sm:flex">Status</p>
--- src/components/pages/settings-page/UsersTableRow.tsx
+++ src/components/pages/settings-page/UsersTableRow.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { UsersDataType as UsersTableRowProps } from "../../../types/UsersDataType";
 import BaseTag from "../../BaseTag";
 import UsersImgStack from "../../UsersImgStack";
@@ -10,12 +11,17 @@ const UsersTableRow = ({
   date,
   status,
   users,
-}: UsersTableRowProps) => {
+  isAllChecked,
+}: UsersTableRowProps & { isAllChecked?: boolean }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
     <div className="flex items-center justify-between p-6.5 border-b border-gray-200 text-gray-500 text-sm">
       <div className="flex gap-3 w-fit sm:w-[25%]">
         <input
           type="checkbox"
+          checked={isChecked || isAllChecked}
+          onChange={(e) => setIsChecked(e.target.checked)}
           className="accent-purple-600 w-5 h-5 rounded-lg border-gray-200 outline-purple-500/20 cursor-pointer"
         />
         <p className="text-gray-900 font-medium">{name}</p>
