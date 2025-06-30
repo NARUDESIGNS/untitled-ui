0eccfe68905ff138d4aaeff560da558927b061e9
Paul <ibeabuchi@worklio.cz>
2025-06-30 08:08:00 +0100

    added table header and table stype

--- src/components/pages/settings-page/UsersTable.tsx
+++ src/components/pages/settings-page/UsersTable.tsx
@@ -21,7 +21,18 @@ const UsersTable = () => {
     <div>
       {isLoading && <p>Loading data...</p>}
       {error && <p>{error.message}</p>}
-      {!isLoading && !error && tableData}
+      {!isLoading && !error && (
+        <div className="bg-white border border-gray-300 shadow-gray-200 shadow-md rounded-lg max-h-[500px] overflow-y-scroll">
+          <div className="flex items-center sticky top-0 justify-between px-6 py-3.25 border-b border-gray-300 text-gray-500 text-sm bg-gray-50 z-10">
+            <p className="w-[25%]">Name</p>
+            <p className="w-[15%]">Type</p>
+            <p className="w-[10%]">Date Created</p>
+            <p className="w-[66px]">Status</p>
+            <p className="w-[30%]">Role Users</p>
+          </div>
+          {tableData}
+        </div>
+      )}
     </div>
   );
 };
